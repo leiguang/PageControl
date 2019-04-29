8865f102e5de523494399b228d372c338c8fb1ac
Guang Lei <575403441@qq.com>
2019-04-29 13:46:09 +0800

    Initial Commit

---
+++ PageControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PageControl
+//
+//  Created by Guang Lei on 2019/4/29.
+//  Copyright © 2019 Guang Lei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
